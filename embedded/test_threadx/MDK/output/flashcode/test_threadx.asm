fe6d7b9970a48e5c2f92d265de295aa2088b13a7
cccc <cuiwei_cv@163.com>
2021-12-29 10:41:05 +0800

    test_threadx: clear-up project files

--- embedded/test_threadx/MDK/output/flashcode/test_threadx.asm
+++ embedded/test_threadx/MDK/output/flashcode/test_threadx.asm
@@ -3,7 +3,7 @@
 
 ** ELF Header Information
 
-    File Name: D:\cccc2020\CODE\workspace\bfsk_8161\embedded\test_threadx\MDK\output\flashcode\adc_03_sa_interrupt.axf
+    File Name: D:\cccc2020\CODE\workspace\bfsk_8161\embedded\test_threadx\MDK\output\flashcode\test_threadx.axf
 
     Machine class: ELFCLASS32 (32-bit)
     Data encoding: ELFDATA2LSB (Little endian)
@@ -31,8 +31,8 @@
     Program header entries: 1
     Section header entries: 15
 
-    Program header offset: 25616 (0x00006410)
-    Section header offset: 25648 (0x00006430)
+    Program header offset: 25596 (0x000063fc)
+    Section header offset: 25628 (0x0000641c)
 
     Section header string table index: 14
 
@@ -694,7 +694,7 @@
 
 
 ** Section #13 '.comment' (SHT_PROGBITS)
-    Size   : 1252 bytes
+    Size   : 1232 bytes
 
 
 ** Section #14 '.shstrtab' (SHT_STRTAB)
