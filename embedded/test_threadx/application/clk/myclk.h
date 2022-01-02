5de34a20272ac3d7320fb6d1c589d918403e9815
cccc <cuiwei_cv@163.com>
2022-01-02 13:00:12 +0800

    test_threadx: clear-up project files

--- embedded/test_threadx/application/clk/myclk.h
+++
@@ -1,9 +0,0 @@
-#ifndef _0F4A2B3F_7CB2_4A80_A3C5_42EFB54EE758_
-#define _0F4A2B3F_7CB2_4A80_A3C5_42EFB54EE758_
-
-extern void SysClkIni(void);
-extern void clk_test(void);
-
-#endif//_0F4A2B3F_7CB2_4A80_A3C5_42EFB54EE758_
-
-
