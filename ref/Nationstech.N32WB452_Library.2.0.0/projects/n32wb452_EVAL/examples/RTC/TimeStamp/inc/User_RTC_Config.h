e38470aef640785a0e690819d482ef353495e751
cccc <cuiwei_cv@163.com>
2022-01-17 16:30:00 +0800

    add tool and update ref and update hardware doc

---
+++ ref/Nationstech.N32WB452_Library.2.0.0/projects/n32wb452_EVAL/examples/RTC/TimeStamp/inc/User_RTC_Config.h
@@ -0,0 +1,29 @@
+#ifndef __USER_RTC_CONFIG_H__
+#define __USER_RTC_CONFIG_H__
+
+typedef enum {
+    RTC_CLK_SRC_TYPE_HSE128=0x01,
+    RTC_CLK_SRC_TYPE_LSE=0x02,
+    RTC_CLK_SRC_TYPE_LSI=0x03,
+}RTC_CLK_SRC_TYPE;
+
+
+extern void RTC_CLKSourceConfig(RTC_CLK_SRC_TYPE Clk_Src_Type, bool Is_First_Cfg_RCC, bool Is_Rst_Bkp);
+extern ErrorStatus RTC_TimeRegulate(void);
+extern ErrorStatus RTC_DateRegulate(void);
+extern ErrorStatus RTC_AlarmRegulate(uint32_t RTC_Alarm);
+extern void RTC_DateAndTimeDefaultVale(void);
+extern  void RTC_PrescalerConfig(void);
+
+extern void RTC_TimeShow(void);
+extern void RTC_DateShow(void);
+extern void RTC_AlarmShow(uint8_t AlarmX);
+extern void EXTI17_RTCAlarm_Configuration(FunctionalState Cmd);
+extern void EXTI_PA7_Configuration(void);
+extern void WakeUpClockSelect(uint8_t WKUPClkSrcSel);
+extern void EXTI20_RTCWKUP_Configuration(FunctionalState Cmd);
+extern void EXTI20_TimeStampIRQn_Configuration(EXTI_TriggerType Trig_Type);
+extern void EXTI_PB8_TimeStamp_Configuration(void);
+extern void RTC_TimeStampShow(void);
+
+#endif/*__USER_RTC_CONFIG_H__*/
