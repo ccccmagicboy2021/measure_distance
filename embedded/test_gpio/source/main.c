4faded15d89b932e34c58a104b17f114469b4326
cccc <cuiwei_cv@163.com>
2022-01-19 17:14:06 +0800

    test_gpio: add led handle

--- embedded/test_gpio/source/main.c
+++ embedded/test_gpio/source/main.c
@@ -1,5 +1,16 @@
 #include "sys.h"
 
+#define PORT_GROUP1 GPIOB
+#define PORT_GROUP2 GPIOA
+
+#define LED1_PORT   PORT_GROUP1
+#define LED2_PORT   PORT_GROUP1
+#define LED3_PORT   PORT_GROUP2
+
+#define LED1_PIN    GPIO_PIN_5
+#define LED2_PIN    GPIO_PIN_4
+#define LED3_PIN    GPIO_PIN_8
+
 void segger_init(void)
 {
 	SEGGER_RTT_Init();
@@ -7,9 +18,82 @@ void segger_init(void)
 	CV_LOG("compiled time: %s %s\r\n", __DATE__, __TIME__);
 }
 
+void LedInit(GPIO_Module* GPIOx, uint16_t Pin)
+{
+    GPIO_InitType GPIO_InitStructure;
+
+    /* Check the parameters */
+    assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
+
+    /* Enable the GPIO Clock */
+    if (GPIOx == GPIOA)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOA, ENABLE);
+    }
+    else if (GPIOx == GPIOB)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOB, ENABLE);
+    }
+    else if (GPIOx == GPIOC)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOC, ENABLE);
+    }
+    else if (GPIOx == GPIOD)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOD, ENABLE);
+    }
+    else if (GPIOx == GPIOE)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOE, ENABLE);
+    }
+    else if (GPIOx == GPIOF)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOF, ENABLE);
+    }
+    else
+    {
+        if (GPIOx == GPIOG)
+        {
+            RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOG, ENABLE);
+        }
+    }
+
+    /* Configure the GPIO pin */
+    if (Pin <= GPIO_PIN_ALL)
+    {
+        GPIO_InitStructure.Pin        = Pin;
+        GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+        GPIO_InitPeripheral(GPIOx, &GPIO_InitStructure);
+    }
+}
+
+void LedOn(GPIO_Module* GPIOx, uint16_t Pin)
+{
+    GPIOx->PBSC = Pin;
+}
+
+void LedOff(GPIO_Module* GPIOx, uint16_t Pin)
+{
+    GPIOx->PBC = Pin;
+}
+
+void LedBlink(GPIO_Module* GPIOx, uint16_t Pin)
+{
+    GPIOx->POD ^= Pin;
+}
+
+void LedOnOff(GPIO_Module* GPIOx, uint32_t Pin)
+{
+    GPIOx->PBSC = Pin;
+}
+
 void led_init(void)
 {
-    //
+    LedInit(PORT_GROUP1, LED1_PIN | LED2_PIN);
+    LedInit(PORT_GROUP2, LED3_PIN);
+    
+    LedOn(PORT_GROUP1, LED1_PIN);
 }
 
 void user_button_init(void)
@@ -30,5 +114,8 @@ int main(void)
 	while(1)
 	{
 		app();
+        LedBlink(PORT_GROUP1, LED1_PIN);
+        LedBlink(PORT_GROUP1, LED2_PIN);
+        LedBlink(PORT_GROUP2, LED3_PIN);
 	}
 }
