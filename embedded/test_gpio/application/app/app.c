adfb5f81e529a1f6393d7da641b7aaf704220276
cccc <cuiwei_cv@163.com>
2022-01-20 10:27:26 +0800

    test_gpio: add button handler

--- embedded/test_gpio/application/app/app.c
+++ embedded/test_gpio/application/app/app.c
@@ -5,13 +5,13 @@ uint16_t m_AdcValue;
 
 void app(void)
 {   
-    char float_str[64];
+    //char float_str[64];
 
     //CV_LOG("adc_raw: %d\r\n", adc_val);           //单采及连续采用
     //printf("adc: %d(%.3lfv)\r\n", adc_val, (float)(((float)adc_val/4095.f)*3.3f));        //单采及连续采用
     //printf("adc: %04d(%.3lfv)\r\n", m_AdcValue, (float)(((float)m_AdcValue/4095.f)*3.3f));        //dma用
-    sprintf(float_str, "adc: %04d(%.3lfv)\r\n", m_AdcValue, (float)(((float)m_AdcValue/4095.f)*3.3f));        //dma用
-    CV_LOG("%s", float_str);
+    //sprintf(float_str, "adc: %04d(%.3lfv)\r\n", m_AdcValue, (float)(((float)m_AdcValue/4095.f)*3.3f));        //dma用
+    //CV_LOG("%s", float_str);
     systick_delay_ms(1000);
 }
 
--- embedded/test_gpio/source/main.c
+++ embedded/test_gpio/source/main.c
@@ -11,6 +11,15 @@
 #define LED2_PIN    GPIO_PIN_4
 #define LED3_PIN    GPIO_PIN_8
 
+#define KEY_INPUT_PORT1 GPIOA
+#define KEY_INPUT_PIN1  GPIO_PIN_4
+
+#define KEY_INPUT_PORT2 GPIOA
+#define KEY_INPUT_PIN2  GPIO_PIN_5
+
+#define KEY_INPUT_PORT3 GPIOA
+#define KEY_INPUT_PIN3  GPIO_PIN_6
+
 void segger_init(void)
 {
 	SEGGER_RTT_Init();
@@ -90,15 +99,69 @@ void LedOnOff(GPIO_Module* GPIOx, uint32_t Pin)
 
 void led_init(void)
 {
+    GPIO_ConfigPinRemap(GPIO_RMP_SW_JTAG_SW_ENABLE, ENABLE);
+    RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_AFIO, ENABLE);
     LedInit(PORT_GROUP1, LED1_PIN | LED2_PIN);
     LedInit(PORT_GROUP2, LED3_PIN);
     
     LedOn(PORT_GROUP1, LED1_PIN);
 }
 
+void KeyInputInit(GPIO_Module* GPIOx, uint16_t Pin)
+{
+    GPIO_InitType GPIO_InitStructure;
+
+    /* Check the parameters */
+    assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
+
+    /* Enable the GPIO Clock */
+    if (GPIOx == GPIOA)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOA, ENABLE);
+    }
+    else if (GPIOx == GPIOB)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOB, ENABLE);
+    }
+    else if (GPIOx == GPIOC)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOC, ENABLE);
+    }
+    else if (GPIOx == GPIOD)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOD, ENABLE);
+    }
+    else if (GPIOx == GPIOE)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOE, ENABLE);
+    }
+    else if (GPIOx == GPIOF)
+    {
+        RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOF, ENABLE);
+    }
+    else
+    {
+        if (GPIOx == GPIOG)
+        {
+            RCC_EnableAPB2PeriphClk(RCC_APB2_PERIPH_GPIOG, ENABLE);
+        }
+    }
+
+    /* Configure the GPIO pin as input floating*/
+    if (Pin <= GPIO_PIN_ALL)
+    {
+        GPIO_InitStructure.Pin        = Pin;
+        GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IPU;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+        GPIO_InitPeripheral(GPIOx, &GPIO_InitStructure);
+    }
+}
+
 void user_button_init(void)
 {
-    //
+    KeyInputInit(KEY_INPUT_PORT1, KEY_INPUT_PIN1);
+    KeyInputInit(KEY_INPUT_PORT2, KEY_INPUT_PIN2);
+    KeyInputInit(KEY_INPUT_PORT3, KEY_INPUT_PIN3);
 }
 
 int main(void)
@@ -114,8 +177,20 @@ int main(void)
 	while(1)
 	{
 		app();
-        LedBlink(PORT_GROUP1, LED1_PIN);
-        LedBlink(PORT_GROUP1, LED2_PIN);
-        LedBlink(PORT_GROUP2, LED3_PIN);
+        if (GPIO_ReadInputDataBit(KEY_INPUT_PORT1, KEY_INPUT_PIN1) == Bit_RESET)
+        {
+            LedBlink(PORT_GROUP1, LED1_PIN);
+            CV_LOG("key1 pressed!\r\n");
+        }
+        if (GPIO_ReadInputDataBit(KEY_INPUT_PORT2, KEY_INPUT_PIN2) == Bit_RESET)
+        {
+            LedBlink(PORT_GROUP1, LED2_PIN);
+            CV_LOG("key2 pressed!\r\n");
+        }
+        if (GPIO_ReadInputDataBit(KEY_INPUT_PORT3, KEY_INPUT_PIN3) == Bit_RESET)
+        {
+            LedBlink(PORT_GROUP2, LED3_PIN);
+            CV_LOG("key3 pressed!\r\n");
+        }
 	}
 }
