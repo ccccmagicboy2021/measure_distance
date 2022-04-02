7a2d2e8388e6b1c27a723582bd2e219926a30911
cccc <cuiwei_cv@163.com>
2022-04-02 16:46:57 +0800

    fsk_distance_measure: use SPEED_LOW_LIMIT1/2 for better result

--- embedded/fsk_distance_measure/app/app/app.c
+++ embedded/fsk_distance_measure/app/app/app.c
@@ -75,7 +75,7 @@ void idle_process(void)
     {
         if (0 < speed_f)
         {
-            if (SPEED_LIMIT1 > fabs(speed_f))
+            if (SPEED_LOW_LIMIT1 < fabs(speed_f))
             {
                 if (MAG_LIMIT1 < mag_f)
                     state = CLOSE;
@@ -92,7 +92,7 @@ void idle_process(void)
     {
         if (0 > speed_f)
         {
-            if (SPEED_LIMIT2 > fabs(speed_f))
+            if (SPEED_LOW_LIMIT2 < fabs(speed_f))
             {
                 if (MAG_LIMIT2 < mag_f)
                     state = LEAVE_S0;
--- embedded/fsk_distance_measure/app/app/app.h
+++ embedded/fsk_distance_measure/app/app/app.h
@@ -2,13 +2,13 @@
 #define _84768E90_ECB2_4533_9ADA_E2781BD43F42_
 
 
-//close: 0.9-1.2m: 1.7
+//close: 0.9-1.2m: 2.0
 
 #define TH_1 (TH_2 - 0.3f)          //unit: meter
 #define TH_2    2.0f                //unit: meter
-#define LEAVING_TIMEOUT 500u        //unit: ms
-#define SPEED_LIMIT1 2.0f           //unit: meter/s
-#define SPEED_LIMIT2 2.0f           //unit: meter/s
+#define LEAVING_TIMEOUT 3000u        //unit: ms
+#define SPEED_LOW_LIMIT1 0.2f           //unit: meter/s
+#define SPEED_LOW_LIMIT2 0.2f           //unit: meter/s
 #define MAG_LIMIT1 5u               //unit: no
 #define MAG_LIMIT2 5u               //unit: no
 
