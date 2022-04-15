175223af7d662bfcb59b62d17362f779537e7f1b
cccc <cuiwei_cv@163.com>
2022-04-15 17:30:39 +0800

    bfskraw/tester: update build script

--- vc/bfskraw/3. build_all.bat
+++ vc/bfskraw/3. build_all.bat
@@ -1,1 +1,16 @@
-MSDEV.EXE burnhex.dsw /make "all - all" /out build_result.txt
+@echo off
+::Keil执行文件位置
+set VC="d:\cccc2020\TOOL\VC++ 6.0\Common\MSDev98\Bin\MSDEV_win81.EXE"
+::查找uvprojx工程文件
+for /f "usebackq delims=" %%j in (`dir /s /b %cd%\*.dsw`) do (
+if exist %%j (
+set VC_PRO_PATH="%%j"))
+echo ---------------------------------------------------------------
+echo Author: cccc
+echo Init building ...
+echo %VC_PRO_PATH%
+echo >build_result.txt
+%VC% %VC_PRO_PATH% /make "all - all" /out %cd%\build_result.txt
+type build_result.txt
+echo Done.
+pause
